7c5f4415b5910512cd70f901a1dde2c350f35bc9
mayuri_gore12 <mayuri.gore@nitorinfotech.com>
2022-01-11 15:02:21 +0530

    fixed Icon changes

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,31 @@
+//Please add the library import below.
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+//Please add the Custom component import below.
+
+//Please add the Custom constants import below.
+
+//Add the constant variables below.
+
+interface props {}
+
+const Dashboard = (props: props) => {
+//If using useState hook place them below
+
+//If using use effests place them below.
+
+//All the custom/business logic function below.
+
+//Make sure to add UnderScore before every custom function you declare (Eg:-_functionName)
+    return (
+        <View>
+            <Text>Test</Text>
+        </View>
+    );
+};
+
+export default Dashboard;
+
+const styles = StyleSheet.create({});
+
