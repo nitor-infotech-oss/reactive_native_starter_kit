095ef469e3f2fe09b5133dc4fa6c3a749e61da97
mayuri_gore12 <mayuri.gore@nitorinfotech.com>
2022-01-11 15:36:30 +0530

    fixed comments

--- src/static/verbiages.ts
+++ src/static/verbiages.ts
@@ -1,5 +1,5 @@
 export const buttonsTitle = {
   generalAPICall: 'API call',
   generalNext: 'Navigate to Next Screen',
-  generalTabButton: ' Navigate to Tabbed Screen',
+  generalTabButton: 'Navigate to Tabbed Screen',
 };
